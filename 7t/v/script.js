e18e38ae4cbb6976a43e51284f4b3634d877742d
Nancysstays <jeremyandnancyforever@gmail.com>
2025-01-29 19:52:23 -0800

    Update script.js

--- 7t/v/script.js
+++ 7t/v/script.js
@@ -54,7 +54,7 @@ async function fetchData() {
         });
 
     } catch (error) {
-        alert(`Error fetching or parsing data: {error}`);
+        consle.log('Error fetching or parsing data:', error);
         errorDiv.style.display = 'block'; // Show error message
     } finally {
         loadingIndicator.style.display = 'none'; // Hide loading indicator
